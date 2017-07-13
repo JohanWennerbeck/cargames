4489c7ee93f6fa304d9b17276a2aba66cf285267
Johan Wennerbeck <Jojjanw@gmail.com>
2017-07-13 14:56:20 +0200

    Get Car Bingo to work

--- app/src/main/java/jaw/minigames/view/adapter/CarBingoAdapter.java
+++ app/src/main/java/jaw/minigames/view/adapter/CarBingoAdapter.java
@@ -5,9 +5,12 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
+import android.widget.Button;
+
+import org.greenrobot.eventbus.EventBus;
 
 import jaw.minigames.R;
+import jaw.minigames.eventbus.TileCheckedEvent;
 import jaw.minigames.model.minigamemodule.carbingogame.ICarBingo;
 import jaw.minigames.model.minigamemodule.carbingogame.ICarBingoTile;
 
@@ -37,13 +40,14 @@ import jaw.minigames.model.minigamemodule.carbingogame.ICarBingoTile;
             View carBingoView;
             CarBingoViewHolder carBingoViewHolder;
 
-            carBingoView = inflater.inflate(R.layout.tile_res, parent, false);
+            carBingoView = inflater.inflate(R.layout.tile_res_simple, parent, false);
             carBingoViewHolder = new CarBingoViewHolder(carBingoView);
             return carBingoViewHolder;
         }
 
         public void onBindViewHolder(CarBingoViewHolder holder, int position) {
             holder.carBingoTile = carBingo.getCarBingoTiles().get(position);
+            holder.i = position;
             holder.setCarBingoTiles();
         }
 
@@ -54,20 +58,57 @@ import jaw.minigames.model.minigamemodule.carbingogame.ICarBingoTile;
 
     public static class CarBingoViewHolder extends RecyclerView.ViewHolder {
             public ICarBingoTile carBingoTile;
-            public ImageView tileImage;
+            //public ImageView tileImage;
+            public int i;
+            public Button button;
 
-            public CarBingoViewHolder(View itemView) {
+            public CarBingoViewHolder(final View itemView) {
                 super(itemView);
-                tileImage = (ImageView) itemView.findViewById(R.id.imageButton);
+                button = (Button) itemView.findViewById(R.id.button);
+
+                button.setOnClickListener(new View.OnClickListener() {
+                    public void onClick(View view){
+                        EventBus.getDefault().post(new TileCheckedEvent(i));
+                        if(carBingoTile.getChecked()){
+                            button.setText("Check");
+                        } else {
+                            button.setText(tileText[i]);
+                        }
+                    }
+                });
+                //tileImage = (ImageView) itemView.findViewById(R.id.imageButton);
             }
 
             public void setCarBingoTiles(){
-                tileImage.setImageResource(mThumbIds[carBingoTile.getType()]);
+                //tileImage.setImageResource(mThumbIds[carBingoTile.getType()]);
+                //button.setText("Cow");
+                button.setText(tileText[carBingoTile.getType()]);
 
                 if(this.carBingoTile.getChecked()){
-                    tileImage.setImageResource(R.drawable.cross);
+                    //tileImage.setImageResource(R.drawable.cross);
                 }
             }
+
+
+
+            private String[] tileText = {
+                    "Ambulance",
+                    "Airplane",
+                    "Bike",
+                    "Boat",
+                    "Church",
+                    "Cow",
+                    "Flag",
+                    "Helicopter",
+                    "Horse",
+                    "McDonalds",
+                    "Police Car",
+                    "Sheep",
+                    "Tractor",
+                    "Train",
+                    "Windmiller",
+                    "Wind turbine",
+            };
 
             private Integer[] mThumbIds = {
                     R.drawable.ambulance,
